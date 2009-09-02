d5ac34467624a6bfe7f3311c7f4ac5398399467e
Mariano Iglesias <mariano@cricava.com>
2009-09-02 12:47:08 -0300

    Moving general validation to EmailAppModel

--- email_app_model.php
+++ email_app_model.php
@@ -6,5 +6,72 @@ class EmailAppModel extends AppModel {
 	 * @var array
 	 */
 	public $actsAs = array('Containable');
+
+	/**
+	 * Validates that at least one of the given fields is not empty
+	 *
+	 * @param array $value Array in the form of $field => $value
+	 * @param array $parameters Parameters ('fields')
+	 * @return bool Success
+	 */
+	public function validateOneNotEmpty($value, $parameters = array()) {
+		reset($value);
+		$field = key($value);
+		$value = array_shift($value);
+		if (empty($parameters['fields'])) {
+			$parameters['fields'] = array($field);
+		}
+
+		$valid = false;
+		foreach((array) $parameters['fields'] as $field) {
+			if (!empty($this->data[$this->alias][$field]) && Validation::notEmpty($this->data[$this->alias][$field])) {
+				$valid = true;
+				break;
+			}
+		}
+		return $valid;
+	}
+
+	/**
+	 * Checks if the value defined is unique for the given data model.
+	 * The check for uniqueness is case-insensitive.  If
+	 * {@link $params}['conditions'] is given, this is used as a constraint.
+	 * If {@link $params}['scope'] is given, the value is only checked against
+	 * records that match the value of the column/field defined by
+	 * {@link $params}['scope'].
+	 *
+	 * @param array $value Array in the form of $field => $value.
+	 * @return bool True if value is unique; false otherwise.
+	 * @access public
+	 */
+	public function validateUnique($value, $params) {
+		$value = array_shift($value);
+		$column = $this->alias . '.' . $params['field'];
+		$id = $this->alias . '.' . $this->primaryKey;
+
+		$conditions = array();
+		if (isset($params['conditions'])) {
+			$conditions = $params['conditions'];
+		}
+
+		if (isset($params['scope'])) {
+			if (is_array($params['scope'])) {
+				foreach ($params['scope'] as $scope) {
+					$conditions[$scope] = $this->data[$this->alias][$scope];
+				}
+			} else if (is_string($params['scope'])) {
+				$conditions[$params['scope']] = $this->data[$this->alias][$params['scope']];
+			}
+		}
+		$conditions[$column] = $value;
+
+		if (isset($this->data[$this->alias][$this->primaryKey])) {
+			$conditions[$id . ' !='] = $this->data[$this->alias][$this->primaryKey];
+		} else if (!empty($this->id)) {
+			$conditions[$id . ' !='] = $this->id;
+		}
+
+		return !$this->hasAny($conditions);
+	}
 }
 ?>
--- models/email_template.php
+++ models/email_template.php
@@ -24,31 +24,6 @@ class EmailTemplate extends EmailAppModel {
 	);
 
 	/**
-	 * Validates that at least one of the given fields is not empty
-	 *
-	 * @param array $value Array in the form of $field => $value
-	 * @param array $parameters Parameters ('fields')
-	 * @return bool Success
-	 */
-	public function validateOneNotEmpty($value, $parameters = array()) {
-		reset($value);
-		$field = key($value);
-		$value = array_shift($value);
-		if (empty($parameters['fields'])) {
-			$parameters['fields'] = array($field);
-		}
-
-		$valid = false;
-		foreach((array) $parameters['fields'] as $field) {
-			if (!empty($this->data[$this->alias][$field]) && Validation::notEmpty($this->data[$this->alias][$field])) {
-				$valid = true;
-				break;
-			}
-		}
-		return $valid;
-	}
-
-	/**
 	 * Checks if value is a valid email layout
 	 *
 	 * @param array $value Array in the form of $field => $value
@@ -61,48 +36,6 @@ class EmailTemplate extends EmailAppModel {
 		}
 
 		return in_array($value, $this->layouts());
-	}
-
-	/**
-	 * Checks if the value defined is unique for the given data model.
-	 * The check for uniqueness is case-insensitive.  If
-	 * {@link $params}['conditions'] is given, this is used as a constraint.
-	 * If {@link $params}['scope'] is given, the value is only checked against
-	 * records that match the value of the column/field defined by
-	 * {@link $params}['scope'].
-	 *
-	 * @param array $value Array in the form of $field => $value.
-	 * @return bool True if value is unique; false otherwise.
-	 * @access public
-	 */
-	public function validateUnique($value, $params) {
-		$value = array_shift($value);
-		$column = $this->alias . '.' . $params['field'];
-		$id = $this->alias . '.' . $this->primaryKey;
-
-		$conditions = array();
-		if (isset($params['conditions'])) {
-			$conditions = $params['conditions'];
-		}
-
-		if (isset($params['scope'])) {
-			if (is_array($params['scope'])) {
-				foreach ($params['scope'] as $scope) {
-					$conditions[$scope] = $this->data[$this->alias][$scope];
-				}
-			} else if (is_string($params['scope'])) {
-				$conditions[$params['scope']] = $this->data[$this->alias][$params['scope']];
-			}
-		}
-		$conditions[$column] = $value;
-
-		if (isset($this->data[$this->alias][$this->primaryKey])) {
-			$conditions[$id . ' !='] = $this->data[$this->alias][$this->primaryKey];
-		} else if (!empty($this->id)) {
-			$conditions[$id . ' !='] = $this->id;
-		}
-
-		return !$this->hasAny($conditions);
 	}
 
 	/**
